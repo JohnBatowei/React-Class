25854635c1565a643f1edf99e962497781225682
JohnBatowei <aritronwebsites@gmail.com>
2025-07-02 16:26:06 +0100

    comp val style

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import FirstComp from "./components/FirstComp.jsx"
 import Navbar  from "./components/Navbar.jsx"
+import './components/styles/home.scss'
 
 function App(){
+
+const EmbeddedStyles = {
+color: 'dodgerblue',
+textDecoration: 'underline'
+}
+
   return (
     <div className="App">
       <Navbar />
       <h1>Adding styles</h1>
+      <h1 style={{color:'red',fontStyle: 'italic'}} >Inline styles</h1>
 
+      <h1 style={EmbeddedStyles}>Embedded styles</h1>
       <FirstComp />
     </div>
   )
